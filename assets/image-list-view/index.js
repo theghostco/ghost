b6072aba827b7c4e925e84abcaebdbeb8566f39a
hulo.dev <59173765+hulo-dev@users.noreply.github.com>
2024-06-25 13:32:29 +0300

    Update index.js

--- assets/image-list-view/index.js
+++ assets/image-list-view/index.js
@@ -1,1 +1,59 @@
-
+!function e(){let i=document.location.origin+"/gallery-content?format=json-pretty",t=document.querySelector(".switcher-gallery-wrapper");if(t){let s=document.createElement("div");s.className="switcher-outer-container",s.innerHTML=`
+<div class="switcher-container">
+<p class="image-view">Image View</p>
+<div class="switcher-block">
+	<div class="switcher"></div>
+</div>
+<p class="list-view">List View</p>
+</div>`,t.append(s);let l=document.createElement("div");l.className="gallery-container";let a=document.createElement("div");a.className="gallery-items",l.append(a),t.append(l);let r=document.createElement("div");r.className="list-container",r.classList.add("hidden-block"),t.append(r);let c=document.createElement("div");c.className="list-items",c.innerHTML=`
+<div class = "list-item first-row">
+  <a class="list-link">
+    <p>Project №</p>
+    <p>Name</p>
+    <p>Location</p>
+  </a>
+</div>`,r.append(c),fetch(i).then(e=>e.json()).then(e=>{for(let i=0;i<e.items.length;i++){let t=e.items[i].assetUrl,s=e.items[i].title,l=e.items[i].excerpt,r=e.items[i].passthrough,d=e.items[i].fullUrl,n=e.items[i].sourceUrl,o=(i+1).toString().padStart(3,"0"),v=document.createElement("div");v.className="gallery-item";let p=document.createElement("div");p.className="list-item",r?(v.innerHTML=`
+<a class="item-link" href="${n}" target="_blank">
+  <div class="item-image">
+      <img src = ${t}>
+  </div>
+<div class="item-decription">
+<div class="item-description-text">
+  <div class="item-header_title">
+      <p>${s}</p>
+  </div>
+  <div class="item-header_title-exerpt">
+      <div><p>${s}</p></div>
+      <div>${l}</div>
+  </div>
+</div>
+<div class="icon-wrapper"><div class="arrow-icon"></div></div>
+</div>
+</a>`,p.innerHTML=`
+<a class="list-link" href="${n}" target="_blank">
+	<p class="list-item_counter">${o}</p>
+	<p>${s}</p>
+    <div>${l}</div>
+</a>`):(v.innerHTML=`
+<a class="item-link" href = "${document.location.origin}${d}">
+  <div class="item-image">
+      <img src = ${t}>
+  </div>
+<div class="item-decription">
+<div class="item-description-text">
+  <div class="item-header_title">
+      <p>${s}</p>
+  </div>
+  <div class="item-header_title-exerpt">
+      <div><p>${s}</p></div>
+      <div>${l}</div>
+  </div>
+</div>
+<div class="icon-wrapper"><div class="arrow-icon"></div></div>
+</div>
+</a>`,p.innerHTML=`
+<a class="list-link" href = "${document.location.origin}${d}">
+	<p class="list-item_counter">00${i+1}</p>
+	<p>${s}</p>
+    <div>${l}</div>
+</a>`),a.append(v),c.append(p)}}).catch(e=>{console.error("Error:",e)})}}(),document.addEventListener("DOMContentLoaded",function(){let e=document.querySelector(".switcher-gallery-wrapper");if(e){let i=e.querySelector(".switcher"),t=e.querySelector(".switcher-container"),s=e.querySelector(".gallery-container"),l=e.querySelector(".list-container");t.addEventListener("click",function(){s.classList.contains("hidden-block")?(s.classList.remove("hidden-block"),l.classList.add("hidden-block"),i.style.left="3px"):(s.classList.add("hidden-block"),l.classList.remove("hidden-block"),i.style.left="calc(100% - 19px)")})}});
