9eced761e10e6d2e8329fac7b5adea25d9c5745d
hulo.dev <59173765+hulo-dev@users.noreply.github.com>
2024-09-12 12:31:26 +0300

    Update back.js

--- assets/announcement-bar-drop-down/back.js
+++ assets/announcement-bar-drop-down/back.js
@@ -1,5 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+<script src="https://www.ghosthub.boo/assets/helpers/jq/jq.js"></script>
 <script>
 const values = {
   	 path: '/page-for-announcement-bar',
@@ -11,6 +10,9 @@ const values = {
 document.addEventListener('DOMContentLoaded', function() {
   setTimeout(() =>{
     if (!document.body.classList.contains('sqs-edit-mode')) {
+      const announcementBar = document.querySelector('.sqs-announcement-bar')
+      const announcementBarBackground = window.getComputedStyle(announcementBar).backgroundColor
+      
       fetch(document.location.origin + values.path)
         .then(response => {
           if (!response.ok) {
@@ -22,18 +24,48 @@ document.addEventListener('DOMContentLoaded', function() {
            const parser = new DOMParser();
            const doc = parser.parseFromString(html, 'text/html');
            const sectionForAnnouncement = doc.querySelector('main .page-section');
+           const sectionForAnnouncementSectionBg = sectionForAnnouncement.querySelector('.section-background')
+           sectionForAnnouncementSectionBg.style.background = announcementBarBackground
            const announcementContent = document.querySelector('.sqs-announcement-bar .sqs-announcement-bar-content')
            const announcementContentText = document.querySelector('.sqs-announcement-bar .sqs-announcement-bar-text')
-           
+         
            const newAnnouncement = document.createElement('div');
            newAnnouncement.className = 'new-announcement-wrapper'
+           newAnnouncement.append(announcementContentText)
            newAnnouncement.append(sectionForAnnouncement)
            
            announcementContent.append(newAnnouncement)
           
            const newAnnouncementSection = document.querySelector('.new-announcement-wrapper section')
-          newAnnouncementSection.prepend(announcementContentText)
-          
+           
+            const announcementText = document.querySelector('.sqs-announcement-bar-text-inner p')
+           const announcementTextColor = window.getComputedStyle(announcementText).color
+           
+           const sectionParagraph = newAnnouncementSection.querySelectorAll('p')
+           sectionParagraph.forEach((p) => {
+           	p.style.color = announcementTextColor
+           })
+        
+        const sectionHeaderL = newAnnouncementSection.querySelectorAll('h1')
+           sectionHeaderL.forEach((h) => {
+           	h.style.color = announcementTextColor
+           })
+        
+        const sectionHeaderSL = newAnnouncementSection.querySelectorAll('h2')
+           sectionHeaderSL.forEach((h) => {
+           	h.style.color = announcementTextColor
+           })
+        
+       const sectionHeaderM = newAnnouncementSection.querySelectorAll('h3')
+           sectionHeaderM.forEach((h) => {
+           	h.style.color = announcementTextColor
+           })
+        
+        const sectionHeaderS = newAnnouncementSection.querySelectorAll('h4')
+           sectionHeaderS.forEach((h) => {
+           	h.style.color = announcementTextColor
+           })
+           
           announcementContentText.addEventListener('click', () => {
           	const newAnnouncementSectionContentWrapper = newAnnouncementSection.querySelector('.content-wrapper')
             announcementContentText.classList.toggle("rotate")
